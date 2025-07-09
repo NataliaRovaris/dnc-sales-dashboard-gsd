2f3baacf9f2b09ac76e3aecabc33717ab45cd953
NataliaRovaris <nataliarovaris@hotmail.com>
2025-07-09 20:35:48 -0300

    Feat: Switch Theme Logic

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
-import { StrictMode } from "react";
-import { ThemeProvider } from "styled-components";
+import { AppThemeProvider } from "./contexts/AppThemeContext.tsx";
 import { createRoot } from "react-dom/client";
+import { GlobalStyle } from "./styles/";
+import { StrictMode } from "react";
 import App from "./App.tsx";
-import { GlobalStyle, lightTheme } from "./styles/";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider theme={lightTheme}>
+    <AppThemeProvider>
       <GlobalStyle />
       <App />
-    </ThemeProvider>
+    </AppThemeProvider>
   </StrictMode>
 );
